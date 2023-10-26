ead0f4cc664d4699ca2ce74d9f9823ed428cd4f5
CharanReddy8 <104820355+CharanReddy8@users.noreply.github.com>
2023-10-26 14:32:47 -0500

    new

---
+++ Team2_App/ForgotViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForgotViewController.swift
+//  Team2_App
+//
+//  Created by Charan Reddy on 10/26/23.
+//
+
+import UIKit
+
+class ForgotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
